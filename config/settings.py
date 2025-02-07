78d7dcdc35039aa529c404e4bf6d20ac1d1bc897
Anvar <anvardiyora.00@gmail.com>
2025-02-07 19:50:44 +0500

    O'zgarishlar qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-load_dotenv()
+
+dotenv_path = Path(__file__).resolve().parent / 'config' / '.env'
+load_dotenv(dotenv_path)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-default-key')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
