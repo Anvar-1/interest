4e3fcfc03dbb19e0ef766147a6e3361b292fdf65
Anvar <anvardiyora.00@gmail.com>
2025-02-19 18:01:18 +0500

    asd

--- config/users/models.py
+++ config/users/models.py
@@ -2,23 +2,25 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Abstra
 from django.db import models
 from rest_framework_simplejwt.tokens import RefreshToken
 
-class UserManager(BaseUserManager):
-    def create_user(self, phone, full_name, password=None, **extra_fields):
-        if not phone:
-            raise ValueError("Foydalanuvchining telefon raqami kerak.")
-        if not full_name:
-            raise ValueError("Foydalanuvchining ismi kerak.")
+class UserRoleManager(BaseUserManager):
 
-        user = self.model(phone=phone, full_name=full_name, **extra_fields)
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError("Telefon raqam kiritilishi shart")
+        extra_fields.setdefault("is_active", True)
+        user = self.model(phone=phone, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, phone, full_name, password=None, **extra_fields):
+    def create_superuser(self, phone, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-
-        return self.create_user(phone, full_name, password, **extra_fields)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser uchun `is_staff=True` bo‘lishi shart")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser uchun `is_superuser=True` bo‘lishi shart")
+        return self.create_user(phone, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -38,11 +40,11 @@ class User(AbstractUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    objects = UserManager()
+    objects = UserRoleManager()
+    username = None
 
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = ['full_name']
-
 
     def tokens(self):
         refresh = RefreshToken.for_user(self)
