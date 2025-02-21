abb10868314610e2c4d4cae74154b4ff3902794a
Anvar <anvardiyora.00@gmail.com>
2025-02-21 15:45:20 +0500

    asd

--- config/qiziqishlar/models.py
+++ config/qiziqishlar/models.py
@@ -13,14 +13,14 @@ class Video(models.Model):
     video_file = models.FileField(upload_to='videos/')
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     interests = models.ManyToManyField(Interest, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # O'zgartirish
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # O'zgartirish
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     interests = models.ManyToManyField(Interest, blank=True)
 
     def __str__(self):
