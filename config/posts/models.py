85d22f5677b729d6ded53eb373656d84108e5059
Anvar <anvardiyora.00@gmail.com>
2025-02-24 11:36:32 +0500

    asd

--- config/posts/models.py
+++ config/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts_user')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
