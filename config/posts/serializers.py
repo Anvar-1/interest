271d8b270ef0a4f41622b4859594d0e40a37af3c
Anvar <anvardiyora.00@gmail.com>
2025-02-24 11:53:49 +0500

    asd

--- config/posts/serializers.py
+++ config/posts/serializers.py
@@ -1,19 +1,28 @@
-from rest_framework import serializers, generics
+from rest_framework import generics
 from .models import Post, Comment
+from .serializers import CommentSerializer
+from ..users import serializers
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['id', 'user', 'content', 'created_at']
 
 class CommentCreateView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def perform_create(self, serializer):
-        post_id = self.kwargs['post_id']
-        post = Post.objects.get(id=post_id)
-        serializer.save(post=post)
+        post_id = self.kwargs['post_id']  # URL dan post_id ni olish
+        post = Post.objects.get(id=post_id)  # Berilgan post ID'sini olish
+        serializer.save(post=post)  # Kommentni saqlash
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'user', 'content', 'created_at']  # 'post' maydonini qo'shish
+        read_only_fields = ['post']  # 'post' maydoni faqat server tomonidan beriladi
+
+
+
 
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
