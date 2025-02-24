625ee1471f44f5ba6b013b0d0289b64c1e5f8c71
Anvar <anvardiyora.00@gmail.com>
2025-02-24 11:34:27 +0500

    asd

---
+++ config/posts/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('config.users.urls')),
     path('', include('config.videoshare.urls')),
     path('', include('config.user_profile.urls')),
+    path('', include('config.posts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
