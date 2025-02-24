91fb854c78c09dc6f50a46b999ad72c499656ff8
Anvar <anvardiyora.00@gmail.com>
2025-02-24 11:45:42 +0500

    asd

--- config/posts/serializers.py
+++ config/posts/serializers.py
@@ -1,10 +1,19 @@
-from rest_framework import serializers
+from rest_framework import serializers, generics
 from .models import Post, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'user', 'content', 'created_at']
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        post = Post.objects.get(id=post_id)
+        serializer.save(post=post)
 
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
